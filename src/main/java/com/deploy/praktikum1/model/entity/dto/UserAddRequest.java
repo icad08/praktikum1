1165cac14121472575d71068d3d032398dcef50b
icad08 <irsyadrm08@gmail.com>
2026-03-04 10:19:33 +0700

    update code in UserAddRequest

--- src/main/java/com/deploy/praktikum1/model/entity/dto/UserAddRequest.java
+++ src/main/java/com/deploy/praktikum1/model/entity/dto/UserAddRequest.java
@@ -1,8 +1,14 @@
 package com.deploy.praktikum1.model.entity.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserAddRequest {
     private String name;
     private int age;
