2a0d36cd038f087a9d6dd5521abcadb13ebb798e
icad08 <irsyadrm08@gmail.com>
2026-03-04 10:00:23 +0700

    add code in UserService

--- src/main/java/com/deploy/praktikum1/service/UserService.java
+++ src/main/java/com/deploy/praktikum1/service/UserService.java
@@ -1,4 +1,16 @@
 package com.deploy.praktikum1.service;
 
+import com.deploy.praktikum1.model.entity.User;
+import com.deploy.praktikum1.model.entity.dto.UserAddRequest;
+import com.deploy.praktikum1.model.entity.dto.UserDto;
+import com.deploy.praktikum1.repository.UserRepository;
+
+import java.util.List;
+
 public interface UserService {
+    UserDto AddUser(UserAddRequest request);
+    List<UserDto> getAllUser();
+    UserDto getUserById(String id);
+    UserDto UpdateUser (String id, UserAddRequest request);
+    void DeleteUser (String id);
 }
